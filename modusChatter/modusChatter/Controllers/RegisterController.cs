c7b61efd09af147630c96c9ce679a5ec11b1ca6d
Munwini Zwane <MZwane@jhb.dvt.co.za>
2016-11-01 15:47:37 +0200

    there you go

---
+++ modusChatter/modusChatter/Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using modusChatter.Models;
+
+namespace modusChatter.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult register(Register reg)
+        {
+           
+            return View();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult login()
+        {
+            return View();
+        }
+    }
+}
---
+++ modusChatter/modusChatter/Models/Register.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using modusChatter.Controllers;
+using modusChatter.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace modusChatter.Models
+{
+    public class Register
+    {
+        [Key]
+
+        [Required(ErrorMessage ="Please enter name",AllowEmptyStrings =false)]
+        public string fullname { get; set; }
+
+       [Required(ErrorMessage ="Please enter password")]
+       [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage ="Please enter username")]
+        public string userName { get; set; }
+
+        [Required(ErrorMessage ="please enter  the programme Field ")]
+        public string programmeTitle { get; set; }
+        
+        [Compare("Password",ErrorMessage = "The password must match")]
+        [DataType(DataType.Password)]
+        public string confirmPassword { get; set; }
+
+        [Required(ErrorMessage ="Please enter valid email address")]
+        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",ErrorMessage ="Enter valid email address")]
+        public string email { get; set; }
+    }
+}
